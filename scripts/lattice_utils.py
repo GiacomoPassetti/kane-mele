8565e66d1f3a8f079347c6dd27be73db746ea20c
Jack <giacomopassetti@hotmail.it>
2023-01-10 16:43:45 +0100

    first version that generates the Hamiltonian for Kane-Melee + EM field. atm only hermiticity works, eigenspectrum and other checks still need to be done

--- scripts/lattice_utils.py
+++ scripts/lattice_utils.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 a = 1
 v1 = a*np.array([np.sqrt(3)/2, 3/2])
 v2 = a*np.array([np.sqrt(3)/2, -3/2])
-n1 = 10
-n2 = 10
+
 rA = a*[0, 0.5]
 rB = a*[0, -0.5]
+
+def I_J_conv(i, j, n):
+    return int((n*i)+j)
 
 def atom_positions_generator(a1, a2, n1, n2, rA, rB):
     positions_array = np.zeros(shape = (n1,n2, 2))
@@ -27,23 +29,98 @@ def unitary_cells_generator(a1, a2, n1, n2, rA, rB):
             positions_array[i, j, :] = ((i*a1)+(j*a2))
     return positions_array
 
-def Hamiltonian_generator(n1, n2):
+def Hamiltonian_generator(n1, n2, t, t_so, b_0):
     number_of_states = n1*n2*4
     Labels = np.ones(shape=(n1, n2))
     indeces_labels = np.column_stack(np.where(Labels==1))
-    H = np.zeros(shape=(number_of_states, number_of_states))
-Hamiltonian_generator(4, 4,)
+    H = np.zeros(shape=(number_of_states, number_of_states), dtype=complex)
+    
+    for i in range(indeces_labels.shape[0]):
+        # Intra-cell Hopping terms
+        H[4*i, 4*i+2] = -t 
+        H[4*i+ 1, 4*i+3] = -t
+
+        # Magnetic Field Term
+        H[4*i, 4*i + 1] = -(1j/2)*b_0 
+        H[4*i+2, 4*i + 3] = -(1j/2)*b_0 
+        
+        try:  # Top Right Next Cell
+          if Labels[tuple(indeces_labels[i]+ np.array([1, 0]))]==1:
+             new_lab = indeces_labels[i]+ np.array([1, 0])
+             j = I_J_conv(new_lab[0], new_lab[1], n1)
+             # Nearest Neighbour hoppings
+             H[4*i, 4*j+2] = -t   # Particle A_up hops to B_up
+             H[4*i + 1, 4*j+3] = -t  # Particle A_down hops to B_down
+             
+             # Next-nearest Neighbour hoppings
+             H[4*i, 4*j+1] = 1j*t_so   # Particle A_up hops to A_down
+             H[4*i + 1, 4*j] = 1j*t_so  # Particle A_down hops to A_up
+             H[4*i+2, 4*j+3] = -1j*t_so   # Particle B_up hops to B_down
+             H[4*i + 3, 4*j + 2] = -1j*t_so  # Particle B_down hops to B_up
+
+
+        except:
+            _=0
+        try:  # Bottom right Next Cell
+          if Labels[tuple(indeces_labels[i]+ np.array([0, 1]))]==1:
+             new_lab = indeces_labels[i]+ np.array([0, 1])
+             j = I_J_conv(new_lab[0], new_lab[1], n1)
+             H[4*i+2, 4*j] = -t   # Particle B_up hops to A_up
+             H[4*i + 3, 4*j+1] = -t  # Particle B_down hops to A_down
+
+             # Next-nearest Neighbour hoppings
+             H[4*i, 4*j+1] = 1j*t_so   # Particle A_up hops to A_down
+             H[4*i + 1, 4*j] = 1j*t_so  # Particle A_down hops to A_up
+             H[4*i+2, 4*j+3] = -1j*t_so   # Particle B_up hops to B_down
+             H[4*i + 3, 4*j + 2] = -1j*t_so  # Particle B_down hops to B_up
+
+        except:
+            _=0
+        try: # Top Left Next cell
+          if (indeces_labels[i]- np.array([0, 1])[0] >= 0) and (indeces_labels[i] - np.array([0, 1])[1] >= 0):
+           if Labels[tuple(indeces_labels[i]- np.array([0, 1]))]==1:
+             new_lab = indeces_labels[i]- np.array([0, 1])
+             j = I_J_conv(new_lab[0], new_lab[1], n1)
+             H[4*i, 4*j+2] = -t   # Particle A_up hops to B_up
+             H[4*i + 1, 4*j+3] = -t  # Particle A_down hops to B_down
+
+             # Next-nearest Neighbour hoppings
+             H[4*i, 4*j+1] = -1j*t_so   # Particle A_up hops to A_down
+             H[4*i + 1, 4*j] = -1j*t_so  # Particle A_down hops to A_up
+             H[4*i+2, 4*j+3] = 1j*t_so   # Particle B_up hops to B_down
+             H[4*i + 3, 4*j + 2] = 1j*t_so  # Particle B_down hops to B_up
+        except:
+            _=0
+        try: # Bottom Left Next cell 
+          if (indeces_labels[i]- np.array([1, 0])[0] >= 0) and (indeces_labels[i] - np.array([1, 0])[1] >= 0):
+           if Labels[tuple(indeces_labels[i]- np.array([1, 0]))]==1:
+             new_lab = indeces_labels[i]- np.array([1, 0])
+             j = I_J_conv(new_lab[0], new_lab[1], n1)
+             H[4*i+2, 4*j] = -t   # Particle B_up hops to A_up
+             H[4*i + 3, 4*j+1] = -t  # Particle B_down hops to A_down
+
+             # Next-nearest Neighbour hoppings
+             H[4*i, 4*j+1] = -1j*t_so   # Particle A_up hops to A_down
+             H[4*i + 1, 4*j] = -1j*t_so  # Particle A_down hops to A_up
+             H[4*i+2, 4*j+3] = 1j*t_so   # Particle B_up hops to B_down
+             H[4*i + 3, 4*j + 2] = 1j*t_so  # Particle B_down hops to B_up
+
+        except:
+            _=0
+
+    H = H + H.transpose().conj()
+    
+    return H 
+
+
+n1 = 2
+n2 = n1
+H = Hamiltonian_generator(n1, n2, 1, 2, 1)
+assert np.allclose(H, H.conj().T)
+print(H, np.min(H))
 
 
 
-
-#pos = atom_positions_generator(v1, v2, n1, n2, rA, rB)
-#fig, ax = plt.subplots()
-#ax.plot(pos[:, 0], pos[:, 1], ls = "", marker = "o")
-#ax.set_xlim(-2, 14)
-#ax.set_ylim(-8, 8)
-#ax.set_aspect('equal')
-#plt.show()
 
  
 
