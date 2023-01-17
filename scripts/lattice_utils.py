3a0267eaea0dcee4b065b35e266501e7246ac1ec
Jack <giacomopassetti@hotmail.it>
2023-01-17 10:19:36 +0100

    adjusted plot_script for density occupation

--- scripts/lattice_utils.py
+++ scripts/lattice_utils.py
@@ -114,6 +114,89 @@ def Hamiltonian_generator(n1, n2, t, t_so, b_0):
     assert np.allclose(H, H.conj().T)
     return H 
 
+def Hamiltonian_boundaries_generator(n1, n2, t, t_so, b_0, Labels):
+    number_of_states = n1*n2*4
+    
+    indeces_labels = np.column_stack(np.where(Labels==1))
+    H = np.zeros(shape=(number_of_states, number_of_states), dtype=complex)
+    
+    for i in range(indeces_labels.shape[0]):
+        # Intra-cell Hopping terms
+        H[4*i, 4*i+2] = -t 
+        H[4*i+ 1, 4*i+3] = -t
+
+        # Magnetic Field Term
+        H[4*i, 4*i + 1] = -1j*b_0 
+        H[4*i+2, 4*i + 3] = -1j*b_0 
+        
+        try:  # Top Right Next Cell
+          if Labels[tuple(indeces_labels[i]+ np.array([1, 0]))]==1:
+             new_lab = indeces_labels[i]+ np.array([1, 0])
+             j = I_J_conv(new_lab[0], new_lab[1], n1)
+             # Nearest Neighbour hoppings
+             H[4*i, 4*j+2] = -t   # Particle A_up hops to B_up
+             H[4*i + 1, 4*j+3] = -t  # Particle A_down hops to B_down
+             
+             # Next-nearest Neighbour hoppings
+             H[4*i, 4*j+1] = 1j*t_so   # Particle A_up hops to A_down
+             H[4*i + 1, 4*j] = 1j*t_so  # Particle A_down hops to A_up
+             H[4*i+2, 4*j+3] = -1j*t_so   # Particle B_up hops to B_down
+             H[4*i + 3, 4*j + 2] = -1j*t_so  # Particle B_down hops to B_up
+
+
+        except:
+            _=0
+        try:  # Bottom right Next Cell
+          if Labels[tuple(indeces_labels[i]+ np.array([0, 1]))]==1:
+             new_lab = indeces_labels[i]+ np.array([0, 1])
+             j = I_J_conv(new_lab[0], new_lab[1], n1)
+             H[4*i+2, 4*j] = -t   # Particle B_up hops to A_up
+             H[4*i + 3, 4*j+1] = -t  # Particle B_down hops to A_down
+
+             # Next-nearest Neighbour hoppings
+             H[4*i, 4*j+1] = 1j*t_so   # Particle A_up hops to A_down
+             H[4*i + 1, 4*j] = 1j*t_so  # Particle A_down hops to A_up
+             H[4*i+2, 4*j+3] = -1j*t_so   # Particle B_up hops to B_down
+             H[4*i + 3, 4*j + 2] = -1j*t_so  # Particle B_down hops to B_up
+
+        except:
+            _=0
+        try: # Top Left Next cell
+          if (indeces_labels[i]- np.array([0, 1])[0] >= 0) and (indeces_labels[i] - np.array([0, 1])[1] >= 0):
+           if Labels[tuple(indeces_labels[i]- np.array([0, 1]))]==1:
+             new_lab = indeces_labels[i]- np.array([0, 1])
+             j = I_J_conv(new_lab[0], new_lab[1], n1)
+             H[4*i, 4*j+2] = -t   # Particle A_up hops to B_up
+             H[4*i + 1, 4*j+3] = -t  # Particle A_down hops to B_down
+
+             # Next-nearest Neighbour hoppings
+             H[4*i, 4*j+1] = -1j*t_so   # Particle A_up hops to A_down
+             H[4*i + 1, 4*j] = -1j*t_so  # Particle A_down hops to A_up
+             H[4*i+2, 4*j+3] = 1j*t_so   # Particle B_up hops to B_down
+             H[4*i + 3, 4*j + 2] = 1j*t_so  # Particle B_down hops to B_up
+        except:
+            _=0
+        try: # Bottom Left Next cell 
+          if (indeces_labels[i]- np.array([1, 0])[0] >= 0) and (indeces_labels[i] - np.array([1, 0])[1] >= 0):
+           if Labels[tuple(indeces_labels[i]- np.array([1, 0]))]==1:
+             new_lab = indeces_labels[i]- np.array([1, 0])
+             j = I_J_conv(new_lab[0], new_lab[1], n1)
+             H[4*i+2, 4*j] = -t   # Particle B_up hops to A_up
+             H[4*i + 3, 4*j+1] = -t  # Particle B_down hops to A_down
+
+             # Next-nearest Neighbour hoppings
+             H[4*i, 4*j+1] = -1j*t_so   # Particle A_up hops to A_down
+             H[4*i + 1, 4*j] = -1j*t_so  # Particle A_down hops to A_up
+             H[4*i+2, 4*j+3] = 1j*t_so   # Particle B_up hops to B_down
+             H[4*i + 3, 4*j + 2] = 1j*t_so  # Particle B_down hops to B_up
+
+        except:
+            _=0
+
+    H = H + H.transpose().conj()
+    assert np.allclose(H, H.conj().T)
+    return H, indeces_labels
+
 
 
 
--- scripts/plot_density_plus_energy.py
+++ scripts/plot_density_plus_energy.py
@@ -4,6 +4,7 @@ from lattice_utils import Hamiltonian_generator, I_J_conv, atom_positions_genera
 from numpy.linalg import eigh
 import matplotlib
 from matplotlib import gridspec
+import matplotlib as mpl
 
 
 a = 1
@@ -13,7 +14,7 @@ v2 = a*np.array([np.sqrt(3)/2, -3/2])
 rA = a*[0, 0.5]
 rB = a*[0, -0.5]
 
-n1 = 16
+n1 = 30
 n2 = n1
 
 t = 1
@@ -25,7 +26,7 @@ b_0 = 0.2
 H = Hamiltonian_generator(n1, n2, t, t_so, b_0)
 u, v = eigh(H)
 
-index_of_state = int(u.shape[0]/2) - 10
+index_of_state = int(u.shape[0]/2) 
 
 selected_state = v[:,index_of_state]
 
@@ -38,20 +39,27 @@ indeces_labels = np.column_stack(np.where(Labels==1))
 
 xs = atom_positions[:, 0]
 ys = atom_positions[:, 1]
-nrow = 1
+nrow = 2
 ncol = 3
-fig = plt.figure(figsize=(5.2, 7), dpi = 800) 
+fig = plt.figure(figsize=(7, 7), dpi = 800) 
 
-gs = gridspec.GridSpec(nrow, ncol, width_ratios = [0.2, 0.1, 1],
+gs = gridspec.GridSpec(nrow, ncol, width_ratios = [0.3, 0.1, 1], height_ratios=[1, 0.05],
+
          wspace=0.0, hspace=0.0, top=0.97, bottom=0.08, left=0.18, right=0.98) 
 ax = plt.subplot(gs[0, 2])
 ax_energy = plt.subplot(gs[0, 0])
+ax_colorbar = plt.subplot(gs[1, 2])
 
 
 
 cmap = matplotlib.cm.get_cmap('OrRd')
+norm = mpl.colors.Normalize(vmin=0, vmax=1)
 
-
+cb1 = mpl.colorbar.ColorbarBase(ax_colorbar, cmap=cmap,
+                                norm=norm,
+                                orientation='horizontal')
+cb1.set_label('P_occupation*10')
+#fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax)
 print(selected_state.shape[0], xs.shape[0])
 for i in range(xs.shape[0]):
     #print("local site occupation: ", (np.abs(selected_state[2*i])**2)+(np.abs(selected_state[2*i + 1])**2))
@@ -59,4 +67,9 @@ for i in range(xs.shape[0]):
 ax.set_aspect("equal")
 ax_energy.plot(np.arange((index_of_state - 10), (index_of_state + 10)), u[(index_of_state - 10):(index_of_state + 10)], marker = "o", ls = "")
 ax_energy.plot(index_of_state, u[index_of_state], color = "red", marker = "o", ls = "")
+ax_energy.set_xlabel("Number of States")
+ax_energy.set_ylabel("E[t]")
+ax.set_xticks([])
+ax.set_yticks([])
+#plt.colorbar()
 plt.savefig("plot_energy_plu_density.png")
